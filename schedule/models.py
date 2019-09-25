087828ec1929aadf76e08ae333b451aa81b6b2a3
ghrimx <debruycker.v@gmail.com>
2019-09-25 09:23:43 +0200

    Update models.py

--- schedule/models.py
+++ schedule/models.py
@@ -17,6 +17,10 @@ from configuration.models import *
 from student.models import Student
 
 class Schedule(models.Model):
+	"""
+	This module allows to keep track of student and staff schedule.
+	StudX is not a scheduler and another program should be used for this purpose.
+	"""
 	__tablename__ = 'Schedule'
 	
 	weekDay = models.IntegerField(choices=DAYS_OF_THE_WEEK)
@@ -24,7 +28,7 @@ class Schedule(models.Model):
 	finishtAt = models.TimeField(null=True, blank=True)
 	location = models.ForeignKey(to='configuration.Location', on_delete=models.SET_NULL, related_name='schedule_classrooms', null=True, blank=True)
 	subject = models.ForeignKey(to='configuration.Subjects', on_delete=models.CASCADE, related_name='schedule_subjects', null=True, blank=True)
-	teacher = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='schedule_teachers')
+	teacher = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='schedule_teachers') # REM.: should be replaced by 'Staff'
 	student = models.ManyToManyField(to='student.Student', related_name='schedule_student', through='Sections', blank=True)
 	status = models.IntegerField(choices=SCHEDULE_STATUS, default=ACTIVE)
 	comment = models.CharField(max_length=255, null=True, blank=True) # Free text
@@ -39,18 +43,18 @@ class Schedule(models.Model):
 		ordering = ('weekDay', 'startAt')
 		verbose_name = _('Schedule')
 		verbose_name_plural = _('Schedule')
-
-'''
-A section is a group of student having the same subject in the same room and at the same time slot of the schedule.
-A section can have student assigned to different "classes".
-Example scenario:
-Classes 1A and 1B have 25 and 10 student respectively. Both classes share common subjects (e.g. Math, science) but have also differents subjects (E.g. 1A has 2hrs of Dutch while 1B has 2hrs of spanish) hence two differents classes.
-However, in order to lower the workload of the teacher having to teach to 25 student at a time, 7 student of classe 1A will have Math with the classe 1B for instance at the same time as the rest of classe 1A has science with another teacher. 
-Two sections are therefore created: one will be 1A1 and the other one will be 1A/1B7 = 1B + 7 student from 1A
-A section allows to accomodate the schedule with maximum of flexibility. 
-It is understood that the content of this table will be of high redundancy with the classe table. 
-'''			
+			
 class Sections(models.Model):
+	'''
+	A section is a group of student having the same subject in the same room and at the same time slot of the schedule.
+	A section can have student assigned to different "classes".
+	Example scenario:
+	Classes 1A and 1B have 25 and 10 student respectively. Both classes share common subjects (e.g. Math, science) but have also differents subjects (E.g. 1A has 2hrs of Dutch while 1B has 2hrs of spanish) hence two differents classes.
+	However, in order to lower the workload of the teacher having to teach to 25 student at a time, 7 student of classe 1A will have Math with the classe 1B for instance at the same time as the rest of classe 1A has science with another teacher. 
+	Two sections are therefore created: one will be 1A1 and the other one will be 1A/1B7 = 1B + 7 student from 1A
+	A section allows to accomodate the schedule with maximum of flexibility. 
+	It is understood that the content of this table will be of high redundancy with the classe table. 
+	'''
 	__tablename__ = 'Section'
 
 	section_name = models.CharField(_('Section'), max_length=45)
